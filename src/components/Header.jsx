ae294038dae962660660af23b57f853c36074bcc
ovie-davet <oviedavid77@mail.com>
2023-04-07 11:45:32 +0100

    done with the initial design for the first section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/img/logo.jpg'
 import { Link } from 'react-router-dom'
 
-import { FaMinus, FaPlus, FaSearchLocation, FaUser, FaFacebookF, FaInstagram} from 'react-icons/fa'
+import { FaMinus, FaPlus, FaSearchLocation, FaUser, FaFacebookF, FaInstagram, FaTimes} from 'react-icons/fa'
 import {BsCart2, BsTwitter} from 'react-icons/bs'
 import {MdEmail} from 'react-icons/md'
 import {ImLocation2} from 'react-icons/im'
+import {BiMenu} from 'react-icons/bi'
 import {FiDribbble} from 'react-icons/fi'
 import { motion } from 'framer-motion'
 
 const Header = () => {
+  const [openMenu, closemenu] = useState(false)
+
+  const menuOpen = () => {
+    closemenu(!openMenu)
+  }
   return (
     <>
-    <div className='flex justify-between items-center px-24 py-2 bg-purple-900 text-white text-sm'>
-        <div className='flex items-center gap-6'>
+    <div className='flex flex-col md:flex-row md:gap-0 justify-between items-center md:px-24 px-2 py-2 bg-purple-900 text-white text-sm'>
+        <div className='hidden md:flex flex-col md:flex-row justify-center text-center items-center md:gap-6 gap-2'>
             <p className='flex items-center gap-2'><span><MdEmail /></span>info@anamonium.com</p>
-            <p className='flex items-center gap-2'><span><ImLocation2 /></span> 23 BDPA, Benin City, Edo State, (NG)</p>
+            <p className='flex md:items-center md:gap-2 gap-[3px] justify-center'><span><ImLocation2 /></span> 23 BDPA, Benin City, Edo State, (NG)</p>
         </div>
-        <div className='flex items-center gap-5'>
+        <div className='flex md:justify-center justify-between md:text-center items-center md:gap-5 gap-28'>
+           <div className="flex gap-5">
            <a href="https://www.facebook.com/"  target="_blank" rel="noreferrer" className='hover:text-gray-400'> <FaFacebookF /></a> 
            <a href="https://www.instagram.com/"  target="_blank" rel="noreferrer" className='hover:text-gray-400'><FaInstagram /> </a>
             <a href="https://www.twitter.com/" target="_blank" rel="noreferrer"  className='hover:text-gray-400'> <BsTwitter /></a>
             <a href="https://www.dribble.com" target="_blank" rel="noreferrer" className='hover:text-gray-400'><FiDribbble /></a>
-            <button className=' before:bg-red-700 before:left-0 before:top-0 before:bottom-0 before:w-full relative before:absolute before:-z-50 z-50 py-2 px-6 rounded-sm ml-5 border border-red-700 hover:before:w-0 before:transition-all before:ease-in-out before:duration-500 active:bg-red-700'>Be an Agent</button>
+           </div>
+            <button className=' before:bg-red-700 before:left-0 before:top-0 before:bottom-0 before:w-full relative before:absolute before:-z-50 z-50 py-2 px-6 rounded-sm md:ml-5 border border-red-700 hover:before:w-0 before:transition-all before:ease-in-out before:duration-500 active:bg-red-700'>Be an Agent</button>
         </div>
     </div>
-    <div className='flex items-center justify-between px-24 py-3 bg-white'>
-      <div className='flex items-center gap-3'>
-        <img src={logo} alt="logo" width="80px" />
-        <h3 className='text-2xl font-bold'>Anamonium</h3>
+    <div className='flex flex-col md:flex-row items-center justify-between md:px-24 px-5 py-3 bg-white'>
+      <div className='flex items-center md:gap-3 gap-1'>
+        <img src={logo} alt="logo" className='md:w-[80px] w-[60px]' />
+        <h3 className='md:text-2xl text-lg font-bold'>Anamonium</h3>
       </div>
-      <ul className='flex items-center justify-center gap-6'>
+      <ul className={`md:flex md:flex-row items-center md:relative justify-center gap-6 ${openMenu === true ? "flex flex-col absolute left-0 bg-white px-6 py-6 z-50 w-[300px]" : " hidden"}`}>
+        <p onClick={() =>  closemenu(false)} className='md:hidden absolute top-3 right-6 text-lg bg-white shadow-md p-2 rounded-full'><FaTimes /></p>
         <li className='group'><a className='text-lg font-semibold text-black hover:text-red-600 transition-colors ease-in-out duration-300 flex items-center gap-2' href="#home"><span>Home</span><FaPlus className='group-hover:hidden font-medium text-sm'/><FaMinus className='hidden group-hover:block font-medium text-sm'/></a></li>
         <li className='group'><a className='text-lg font-semibold text-black hover:text-red-600 transition-colors ease-in-out duration-300 flex items-center gap-2' href="#About"><span>About</span><FaPlus className='group-hover:hidden font-medium text-sm'/><FaMinus className='hidden group-hover:block font-medium text-sm'/></a></li>
         <li className='group'><a className='text-lg font-semibold text-black hover:text-red-600 transition-colors ease-in-out duration-300 flex items-center gap-2' href="#find"><span>Find</span><FaPlus className='group-hover:hidden font-medium text-sm'/><FaMinus className='hidden group-hover:block font-medium text-sm'/></a></li>
@@ -43,6 +52,13 @@ const Header = () => {
         <span className={`hidden`}><input type="search" /></span>
         <motion.span  whileHover={{ scale: 0.9 }} className=' cursor-pointer bg-white shadow-md p-2 text-xl'><FaUser /></motion.span>
         <motion.span  whileHover={{ scale: 0.9 }} className=' cursor-pointer bg-white shadow-md p-2 text-xl'><BsCart2/></motion.span>
+        <motion.span 
+        onClick={menuOpen}
+        whileHover={{ scale: 0.9 }} className=' cursor-pointer bg-white shadow-md p-2 text-xl block md:hidden'>
+          {
+            openMenu === true ? <FaTimes /> : <BiMenu />
+          }
+        </motion.span>
       </div>
     </div>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaHome, FaPlay } from 'react-icons/fa'
+import home from '../assets/img/21.png'
+import { motion } from 'framer-motion'
 
 function Home() {
+  const [typo, setTypo] = useState('one')
+
+
+  const changeTypo = (id) => {
+    setTypo(id)
+  }
+
+
+
+
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className='flex flex-col justify-center align-middle items-center py-12'>
+      <div
+        className={`flex justify-between md:flex-row flex-col items-center md:px-24 px-5 gap-20 transition ease-in-out duration-300 ${typo === 'two' ? 'md:flex-row-reverse flex-col ' : 'row transition ease-in-out duration-300'}`}>
+          <div className={` ${typo === 'two'? 'text-right items-end': ''} flex flex-col gap-2 md:w-[40%] w-[100%]`}>
+            <motion.p
+              initial={{ translateY: 150, opacity: 0 }}
+              whileInView={{ translateY: 0, opacity: 1 }}
+              transition={{
+                ease: "linear",
+                duration: 1,
+              }}
+            className=' flex gap-1 font-bold items-center md:text-xl'><span className='text-red-700 md:text-2xl'><FaHome /></span>Student Hostel Portal</motion.p>
+            <motion.h1
+             initial={{ translateY: 150, opacity: 0 }}
+             whileInView={{ translateY: 0, opacity: 1 }}
+             transition={{
+               ease: "linear",
+               duration: 1.5,
+             }}
+            className='md:text-6xl text-3xl font-extrabold'>FIND A HOSTEL WITH EASE</motion.h1>
+            <motion.p
+             initial={{ translateY: 150, opacity: 0 }}
+             whileInView={{ translateY: 0, opacity: 1 }}
+             transition={{
+               ease: "linear",
+               duration: 2,
+             }}
+            className={` ${typo === 'two'? "pl-[unset] before:right-0 before:left-[unset] md:w-[90%] " : ''} md:w-[80%] text-gray-600 pl-8  before:bg-gray-700 before:absolute relative before:top-0 before:bottom-0 before:left-0 before:w-[1px] pr-10 py-1 my-4`}>You dont't need that stress, we connect you to closeby and affordable hostels</motion.p>
+            <motion.div
+             initial={{ translateY: 150, opacity: 0 }}
+             whileInView={{ translateY: 0, opacity: 1 }}
+             transition={{
+               ease: "linear",
+               duration: 2.5,
+             }}
+            className="flex items-center gap-6">
+              <button className='py-3 px-6 border-red-700 border text-white before:bg-red-700 before:left-0 before:top-0 before:bottom-0 before:w-full relative before:absolute before:-z-50 z-50 hover:before:w-0 hover:text-red-700 before:transition-all before:ease-in-out before:duration-500 active:bg-red-700'>Make an Enquiry</button>
+              <motion.button
+              initial={{scale: 0.95}}
+              animate={{scale: 1.1}}
+              transition={{
+                ease: "linear",
+                duration: .5,
+                repeat: "infinity"
+              }}
+              className={`bg-white shadow-lg w-[40px] h-[40px] rounded-full text-red-700 p-1 flex items-center justify-center text-xl  ${typo === 'two' ? "hidden" : ''} `}><FaPlay /></motion.button>
+              <button className={` ${typo === 'two' ? "block": "hidden"} border border-gray-500 py-3 px-6  before:bg-red-700 before:left-0 before:top-0 before:bottom-0 before:w-0 relative before:absolute before:-z-50 z-50 hover:before:w-full hover:text-white before:transition-all before:ease-in-out before:duration-500 active:bg-red-700`}>Learn More</button>
+            </motion.div>
+          </div>
+          <motion.div className='md:w-[40%] w-[100%]'
+          initial={{ translateX: 150 }}
+          whileInView={{ translateX: 0 }}
+          transition={{
+            ease: "linear",
+            duration: 2,
+          }}
+          >
+            <motion.img
+            initial={{ opacity: 0 }} 
+            animate={{ opacity: 1 }} 
+            transition={{ duration: 0.5 }}
+            src={home} width="100%" alt="home" className='' />
+          </motion.div>
+      </div>
+      <div className='mt-6 flex items-center gap-2'>
+        <span id="one" onClick={() =>changeTypo('one')} className={` ${typo === 'one' ? 'bg-red-700 h-[20px] w-[20px] border-none': ''} bg-gray-500 border border-gray-600 w-[15px] h-[15px] rounded-full cursor-pointer`}></span>
+        <span onClick={() =>changeTypo('two')} id="two" className={` ${typo === 'two' ? 'bg-red-700 h-[20px] w-[20px] border-none': ''} bg-gray-500 border border-gray-600 w-[15px] h-[15px] rounded-full cursor-pointer`}></span>
+      </div>
+  </div>
   )
 }
 
